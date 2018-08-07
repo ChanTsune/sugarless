3600111705b1d29c43d82bc7497177c2106de924
taiki tsunekawa <s1f101702431@iniad.org>
2018-08-07 17:17:23 +0900

    refactoring
    Functionalization of common parts

--- sugarless.hpp
+++ sugarless.hpp
@@ -13,10 +13,10 @@ enum arg_type{
     SPACE_TYPE
 };
 
+typedef std::tuple<std::regex, std::regex, std::string, bool, std::string, bool, std::string> cmd_tuple;
 class Command{
   private:
-    int arg_type_m;
-    std::unordered_map<std::string,std::tuple<std::regex,std::regex,std::string,bool,std::string,bool,std::string >> flag_items_m;
+    std::unordered_map<std::string, cmd_tuple > flag_items_m;
     std::string license_txt_m;
     enum flag_index
     {
@@ -28,15 +28,29 @@ class Command{
         IS_EXIST,
         ARG_VAL
     };
+    bool isLongName(const std::string &_Str,std::smatch &_Re)
+    {
+        return std::regex_match(_Str, _Re, std::regex("--(.+?)(=(.+))?"));
+    }
+    bool isShortName(const std::string &_Str,std::smatch &_Re)
+    {
+        return std::regex_match(_Str, _Re, std::regex("-(.)(=(.+))?"));
+    }
+    bool isCompoundShortName(const std::string &_Str, std::smatch &_Re)
+    {
+        return std::regex_match(_Str, _Re, std::regex("-(.+?)(=(.+))?"));
+    }
     bool __simple_string_parse(std::string &simple_string);
     bool __compound_short_name_parse(std::string &compound_short_name);
     bool _equal_parse(int argc, const char *argv[]);
+    bool __equalArgSet(cmd_tuple &_Tuple, std::string &in_argv_i, std::string &target_arg);
     bool _space_parse(int argc, const char *argv[]);
+    bool __spaceArgSet(cmd_tuple &_Tuple, int argc, const char *argv[], int &i, std::string &in_argv_i);
 
   public:
     Command(void){};
-    Command(std::string &license_text);
-    Command(std::string &&license_text);
+    Command(std::string &license_text) : license_txt_m(license_text) {}
+    Command(std::string &&license_text) : license_txt_m(license_text) {}
     bool parse(int argc, const char *argv[], int arg_type=SPACE_TYPE);
     Command &flag(std::string tag_name,std::vector<char> short_name={},std::vector<std::string> long_name={},std::string description_message="",bool need_arg=false,std::string default_val="");
     bool has(std::string tag_name);
@@ -54,14 +68,7 @@ class Command{
 };
 
 inline bool accessable_next_argv(int argc,int now){
-    return argc > now+1;
-}
-
-Command::Command(std::string &license_text){
-    license_txt_m = license_text;
-}
-Command::Command(std::string &&license_text){
-    license_txt_m = license_text;
+    return argc > now + 1;
 }
 
 bool Command::parse(int argc,const char *argv[],int arg_type){
@@ -73,7 +80,7 @@ bool Command::parse(int argc,const char *argv[],int arg_type){
         case SPACE_TYPE:
             return _space_parse(argc,argv);
         default:
-            fprintf_s(stderr, "sugarless::Command::parse : received an invalid value for the third argument.");
+            fprintf_s(stderr, "sugarless::Command::parse : received an invalid %d value for the third argument.",arg_type);
             break;
     }
     return 0;
@@ -92,8 +99,81 @@ bool Command::__simple_string_parse(std::string &simple_string){
     return 0;
 }
 
-bool Command::__compound_short_name_parse(std::string &compound_short_name){
-    for (int i=0;i<compound_short_name.size();++i)
+bool Command::__equalArgSet(cmd_tuple &_Tuple, std::string &target, std::string &target_arg)
+{
+    if (std::get<NEED_ARG>(_Tuple)) //引数を必要とするオプションか確認
+    {
+        if (target_arg.empty())
+        {
+            if (std::get<DEF_VAL>(_Tuple).empty())
+            {
+                fprintf(stderr, "%s option need argment.\n", target.c_str());
+                return 0;
+            }
+            else
+            {
+                std::get<ARG_VAL>(_Tuple) = target_arg;
+            }
+        }
+        else
+        {
+            std::get<ARG_VAL>(_Tuple) = target_arg;
+        }
+    }
+    else
+    {
+        if (!target_arg.empty())
+        {
+            fprintf(stderr, "%s option do not need argment.\n", target.c_str());
+            return 0;
+        }
+    }
+    return 1;
+}
+
+bool Command::__spaceArgSet(cmd_tuple &_Tuple,int argc,const char*argv[],int &i,std::string &in_argv_i)
+{
+    if (std::get<NEED_ARG>(_Tuple)) //引数を必要とするオプションか確認
+    {
+        if (accessable_next_argv(argc, i)) //次の要素にアクセスできるか確認
+        {                                  //できる場合
+            std::string next_arg(argv[++i]);
+            if (std::regex_match(next_arg, std::regex("-(.+)"))) //引数パラメータの形をとっているか確認
+            {                                                    //-X又は--xxの形式をとっていた場合
+                if (std::get<DEF_VAL>(_Tuple).empty())
+                { //デフォルト引数が設定されていない場合
+                    fprintf_s(stderr, "%s option need argment.\n", in_argv_i.c_str());
+                    return 0;
+                }
+                else
+                { //されている場合
+                    std::get<ARG_VAL>(_Tuple) = std::get<DEF_VAL>(_Tuple);
+                }
+            }
+            else
+            { //-や--で始まらない通常文字列の場合
+                std::get<ARG_VAL>(_Tuple) = next_arg;
+            }
+        }
+        else
+        { //出来ない場合
+            if (std::get<DEF_VAL>(_Tuple).empty())
+            { //デフォルト引数が設定されていない場合
+                fprintf_s(stderr, "%s option need argment.\n", in_argv_i.c_str());
+                return 0;
+            }
+            else
+            { //されている場合
+                std::get<ARG_VAL>(_Tuple) = std::get<DEF_VAL>(_Tuple);
+            }
+        }
+    }
+    return 1;
+}
+
+bool Command::__compound_short_name_parse(std::string &compound_short_name)
+{
+    for (int i = 0; i < compound_short_name.size(); ++i)
     {
         std::string target = {compound_short_name[i]};
         for (auto &f : flag_items_m)
@@ -119,123 +199,54 @@ bool Command::__compound_short_name_parse(std::string &compound_short_name){
     return 1;
 }
 
-bool Command::_space_parse(int argc, const char *argv[]){
-    
-    for(size_t i = 1; i < argc; i++)
+bool Command::_space_parse(int argc, const char *argv[])
+{
+
+    for(int i = 1; i < argc; i++)
     {
         std::string in_argv_i(argv[i]);
         std::smatch sub_much;
-        if (std::regex_match(in_argv_i,sub_much,std::regex("--(.+)")))//long name
+        if (isLongName(in_argv_i,sub_much))//long name
         {
             std::string target = sub_much.str(1);//マッチさせる対象
-            int counter = flag_items_m.size();
             for (auto &f : flag_items_m)//ここのループに改善の余地あり
             {
                 if (std::regex_match(target, std::get<LONG_NAME>(f.second)))//long nameで設定した文字列のいずれかに当てはまるか確認
                 {
-                    if (std::get<NEED_ARG>(f.second)) //引数を必要とするオプションか確認
+                    if(!__spaceArgSet(f.second,argc,argv,i,in_argv_i))
                     {
-                        if(accessable_next_argv(argc,i))//次の要素にアクセスできるか確認
-                        {//できる場合
-                            std::string next_arg(argv[i+1]);
-                            if (std::regex_match(next_arg, std::regex("-(.+)"))) //引数パラメータの形をとっているか確認
-                            {//-X又は--xxの形式をとっていた場合
-                                if (std::get<DEF_VAL>(f.second).empty())
-                                { //デフォルト引数が設定されていない場合
-                                    fprintf_s(stderr, "--%s option need argment.\n",in_argv_i.c_str());
-                                    return 0;
-                                }
-                                else
-                                { //されている場合
-                                    std::get<ARG_VAL>(f.second) = std::get<DEF_VAL>(f.second);
-                                }
-                            }
-                            else
-                            {//-や--で始まらない通常文字列の場合
-                                std::get<ARG_VAL>(f.second) = next_arg;
-                            }
-                        }
-                        else
-                        {//出来ない場合
-                            if (std::get<DEF_VAL>(f.second).empty())
-                            {//デフォルト引数が設定されていない場合
-                                fprintf_s(stderr, "--%s option need argment.\n",in_argv_i.c_str());
-                                return 0;
-                            }
-                            else
-                            {//されている場合
-                                std::get<ARG_VAL>(f.second) = std::get<DEF_VAL>(f.second);
-                            }                            
-                        }
+                        return 0;
                     }
                     std::get<IS_EXIST>(f.second) = true;
                 }
-                else
-                {
-                    ++counter;
-                    if (counter == flag_items_m.size()) {
-                        fprintf_s(stderr, "--%s option is not exist.\n", in_argv_i.c_str());
-                        return 0;
-                    }
-                }
             }
         }
-        else if(std::regex_match(in_argv_i,sub_much,std::regex("-(.)")))//short name
+        else if(isShortName(in_argv_i,sub_much))//short name
         {
             std::string target = sub_much.str(1);//マッチさせる対象
             for(auto &f : flag_items_m)
             {
                 if (std::regex_match(target, std::get<SHORT_NAME>(f.second)))
                 {
-                    if (std::get<NEED_ARG>(f.second)) //引数を必要とするオプションか確認
+                    if (!__spaceArgSet(f.second, argc, argv, i, in_argv_i))
                     {
-                        if(accessable_next_argv(argc,i))//次の要素にアクセスできるか確認
-                        {//できる場合
-                            std::string next_arg(argv[i+1]);
-                            if (std::regex_match(next_arg, std::regex("-(.+)"))) //引数パラメータの形をとっているか確認
-                            {//-X又は--xxの形式をとっていた場合
-                                if (std::get<DEF_VAL>(f.second).empty())
-                                { //デフォルト引数が設定されていない場合
-                                    fprintf_s(stderr, "-%s option need argment.\n",in_argv_i.c_str());
-                                    return 0;
-                                }
-                                else
-                                { //されている場合
-                                    std::get<ARG_VAL>(f.second) = std::get<DEF_VAL>(f.second);
-                                }
-                            }
-                            else
-                            {//-や--で始まらない通常文字列の場合
-                                std::get<ARG_VAL>(f.second) = next_arg;
-                            }                            
-                        }
-                        else
-                        {//出来ない場合
-                            if (std::get<DEF_VAL>(f.second).empty())
-                            {//デフォルト引数が設定されていない場合
-                                fprintf_s(stderr, "-%s option need argment.\n",in_argv_i.c_str());
-                                return 0;
-                            }
-                            else
-                            {//されている場合
-                                std::get<ARG_VAL>(f.second) = std::get<DEF_VAL>(f.second);
-                            }                            
-                        }
+                        return 0;
                     }
                     std::get<IS_EXIST>(f.second) = true;
                 }
             }
         }
-        else if(std::regex_match(in_argv_i,sub_much,std::regex("-(.+)")))//複合short name
+        else if(isCompoundShortName(in_argv_i,sub_much))//複合short name
         {
-            if(!__compound_short_name_parse(sub_much.str(1))){
+            if(!__compound_short_name_parse(sub_much.str(1)))
+            {
                 return 0;
             }
         }
         else//simple string
         {
-
-            if (!__simple_string_parse(in_argv_i)){
+            if (!__simple_string_parse(in_argv_i))
+            {
                 return 0;
             }
         }
@@ -245,11 +256,11 @@ bool Command::_space_parse(int argc, const char *argv[]){
 
 bool Command::_equal_parse(int argc, const char *argv[]){
     
-    for(size_t i = 1; i < argc; i++)
+    for(int i = 1; i < argc; i++)
     {
         std::string in_argv_i(argv[i]);
         std::smatch sub_much;
-        if (std::regex_match(in_argv_i, sub_much, std::regex("--(.+?)(=(.+))?"))) //long
+        if (isLongName(in_argv_i, sub_much)) //long
         {
             std::string target = sub_much.str(1);
             std::string target_arg = sub_much.str(3);
@@ -257,38 +268,15 @@ bool Command::_equal_parse(int argc, const char *argv[]){
             {
                 if (std::regex_match(target, std::get<LONG_NAME>(f.second))) //long nameで設定した文字列のいずれかに当てはまるか確認
                 {
-                    if (std::get<NEED_ARG>(f.second)) //引数を必要とするオプションか確認
+                    if (!__equalArgSet(f.second, target, target_arg))
                     {
-                        if(target_arg.empty())
-                        {
-                            if(std::get<DEF_VAL>(f.second).empty())
-                            {
-                                fprintf(stderr,"--%s option need argment.\n",target.c_str());
-                                return 0;
-                            }
-                            else
-                            {
-                                std::get<ARG_VAL>(f.second) = target_arg;
-                            }
-                        }
-                        else
-                        {
-                            std::get<ARG_VAL>(f.second) = target_arg;
-                        }
-                    }
-                    else
-                    {
-                        if(!target_arg.empty())
-                        {
-                            fprintf(stderr,"--%s option do not need argment.\n",target.c_str());
-                            return 0;
-                        }
+                        return 0;
                     }
                     std::get<IS_EXIST>(f.second) = true;
                 }
             }
         }
-        else if (std::regex_match(in_argv_i, sub_much, std::regex("-(.)(=(.+))?"))) //short
+        else if (isShortName(in_argv_i, sub_much)) //short
         {
             std::string target = sub_much.str(1); //マッチさせる対象
             std::string target_arg = sub_much.str(3);
@@ -296,46 +284,26 @@ bool Command::_equal_parse(int argc, const char *argv[]){
             {
                 if (std::regex_match(target, std::get<SHORT_NAME>(f.second)))
                 {
-                    if (std::get<NEED_ARG>(f.second)) //引数を必要とするオプションか確認
+                    if (!__equalArgSet(f.second,target,target_arg))
                     {
-                        if (target_arg.empty())
-                        {
-                            if (std::get<DEF_VAL>(f.second).empty())
-                            {
-                                fprintf(stderr, "-%s option need argment.\n",target.c_str());
-                                return 0;
-                            }
-                            else
-                            {
-                                std::get<ARG_VAL>(f.second) = target_arg;
-                            }
-                        }
-                        else
-                        {
-                            std::get<ARG_VAL>(f.second) = target_arg;
-                        }
-                    }
-                    else
-                    {
-                        if (!target_arg.empty())
-                        {
-                            fprintf(stderr, "-%s option do not need argment.\n", target.c_str());
-                            return 0;
-                        }
+                        return 0;
                     }
                     std::get<IS_EXIST>(f.second) = true;
                 }
             }
         }
-        else if (std::regex_match(in_argv_i, sub_much, std::regex("-(.+)(=(.+))?"))) //compound short
+        else if (isCompoundShortName(in_argv_i, sub_much)) //compound short
         {
-            if (!sub_much.str(3).empty())
+            if (sub_much.str(3).empty())
             {
-                fprintf(stderr,"%s can not given argment.\n",in_argv_i.c_str());
-                return 0;
+                if (!__compound_short_name_parse(sub_much.str(1)))
+                {
+                    return 0;
+                }
             }
-            if (!__compound_short_name_parse(sub_much.str(1)))
+            else
             {
+                fprintf(stderr, "%s can not given argment.\n", in_argv_i.c_str());
                 return 0;
             }
         }
