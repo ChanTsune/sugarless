e68ce38f60a79774be72de4f88338799b3c462c3
taiki tsunekawa <s1f101702431@iniad.org>
2018-08-09 12:40:25 +0900

    Support for UNIX style parsing
    In this commit, the following changes were made.
    
    ・Change argument analysis name (TYPE to STYLE)
    ・Supporting argument receipt beginning with - by using "--" (Conform to UNIX)
    ・Supports UNIX style argument acceptance
    ・Changed the parse method to UNIX style by default

--- example.cpp
+++ example.cpp
@@ -5,12 +5,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     sugarless::Command cmd = sugarless::Command();
-                            cmd.flag("in_file");
+                            cmd.argument("in_file");
                             cmd.flag("overwrite",{'w','r'},{"overwrite"},"overwrite input file")
                             .flag("output",{'o'},{"output"},"output file name",true,"./a.out")
                             .flag("max",{'M'},{"max"},"max size",true);
 
-    if(!cmd.parse(argc,argv,sugarless::arg_type::EQUAL_TYPE)){
+    if(!cmd.parse(argc,argv,sugarless::arg_style::UNIX_STYLE)){
         cout << "parse error" << endl;
     }
 
@@ -26,6 +26,9 @@ int main(int argc, char const *argv[])
     }
     if(cmd.has("output")){
         cout << cmd.get<string>("output") << endl;
+    }
+    if(cmd.has("max")){
+        cout << "max is" << cmd.get<string>("max") << endl;
     }
 
     return 0;
--- sugarless.hpp
+++ sugarless.hpp
@@ -9,16 +9,19 @@
 #include <regex>
 
 namespace sugarless{
-enum arg_type{
-    EQUAL_TYPE,
-    SPACE_TYPE
+enum arg_style{
+    EQUAL_STYLE,
+    SPACE_STYLE,
+    UNIX_STYLE
 };
 
 typedef std::tuple<std::regex, std::regex, std::string, bool, std::string, bool, std::string> cmd_tuple;
 class Command{
   private:
     std::unordered_map<std::string, cmd_tuple > flag_items_m;
-    std::string license_txt_m;
+    std::string app_name_m;
+    std::string arguments_text_m;
+    std::string options_text_m;
     enum flag_index
     {
         SHORT_NAME,
@@ -51,43 +54,50 @@ class Command{
     template<size_t INDEX>
     bool __spaceIsMatch(std::string &target_Str, int argc, const char *argv[], int &i, std::string &in_argv_i);
     bool __spaceArgSet(cmd_tuple &_Tuple, int argc, const char *argv[], int &i, std::string &in_argv_i);
+    bool _unix_parse(int argc,const char *argv[]);
+    void auto_help(void);
 
   public:
-    Command(void){};
-    Command(std::string &license_text) : license_txt_m(license_text) {}
-    Command(std::string &&license_text) : license_txt_m(license_text) {}
-    bool parse(int argc, const char *argv[], int arg_type=SPACE_TYPE);
-    Command &flag(std::string tag_name,std::initializer_list<char> short_name={},std::initializer_list<std::string> long_name={},std::string description_message="",bool need_arg=false,std::string default_val="");
+    Command(bool auto_help=true){};
+    Command(std::string &app_name,bool auto_help=true) : app_name_m(app_name) {}
+    Command(std::string &&app_name,bool auto_help=true) : app_name_m(app_name) {}
+    bool parse(int argc, const char *argv[], int arg_style=UNIX_STYLE);
+    Command &argument(std::string tag_name, std::string description_message="",std::string default_val="");
+    Command &flag(std::string tag_name, std::initializer_list<char> short_name={}, std::initializer_list<std::string> long_name={}, std::string description_message="", bool need_arg=false, std::string default_val="");
     bool has(std::string tag_name);
     template<class T>
     T get(std::string tag_name);
     //====//
-    void show_help(bool with_license=false);
+    void show_help();
     std::string get_help(void);
     void get_help(std::string &dst);
-    void show_license(void);
-    std::string get_license(void);
-    void get_license(std::string &dst);
     //====//
 
 };
+
+void Command::auto_help(void)
+{
+    flag("help",{'h'},{"help"},"show this message.");
+}
 
 inline bool accessable_next_argv(int argc,int now)
 {
     return argc > now + 1;
 }
 
-bool Command::parse(int argc,const char *argv[],int arg_type)
+bool Command::parse(int argc,const char *argv[],int arg_style)
 {
 
-    switch (arg_type)
+    switch (arg_style)
     {
-        case EQUAL_TYPE:
+        case EQUAL_STYLE:
             return _equal_parse(argc,argv);
-        case SPACE_TYPE:
+        case SPACE_STYLE:
             return _space_parse(argc,argv);
+        case UNIX_STYLE:
+            return _unix_parse(argc,argv);
         default:
-            fprintf_s(stderr, "sugarless::Command::parse : received an invalid %d value for the third argument.",arg_type);
+            fprintf_s(stderr, "sugarless::Command::parse : received an invalid %d value for the third argument.",arg_style);
             break;
     }
     return 0;
@@ -115,7 +125,7 @@ bool Command::__equalArgSet(cmd_tuple &_Tuple, std::string &target, std::string 
         {
             if (std::get<DEF_VAL>(_Tuple).empty())
             {
-                fprintf(stderr, "%s option need argment.\n", target.c_str());
+                fprintf(stderr, "%s option need argument.\n", target.c_str());
                 return 0;
             }
             else
@@ -132,7 +142,7 @@ bool Command::__equalArgSet(cmd_tuple &_Tuple, std::string &target, std::string 
     {
         if (!target_arg.empty())
         {
-            fprintf(stderr, "%s option do not need argment.\n", target.c_str());
+            fprintf(stderr, "%s option do not need argument.\n", target.c_str());
             return 0;
         }
     }
@@ -146,11 +156,22 @@ bool Command::__spaceArgSet(cmd_tuple &_Tuple,int argc,const char*argv[],int &i,
         if (accessable_next_argv(argc, i)) //次の要素にアクセスできるか確認
         {                                  //できる場合
             std::string next_arg(argv[++i]);
-            if (std::regex_match(next_arg, std::regex("-(.+)"))) //引数パラメータの形をとっているか確認
+            if(next_arg == "--")
+            {
+                if(accessable_next_argv(argc,i))
+                {
+                    std::get<ARG_VAL>(_Tuple) = std::string(argv[++i]);
+                }
+                else
+                {
+                    fprintf_s(stderr, "Invalid '--' received.");
+                }
+            }
+            else if (std::regex_match(next_arg, std::regex("-(.+)"))) //引数パラメータの形をとっているか確認
             {                                                    //-X又は--xxの形式をとっていた場合
                 if (std::get<DEF_VAL>(_Tuple).empty())
                 { //デフォルト引数が設定されていない場合
-                    fprintf_s(stderr, "%s option need argment.\n", in_argv_i.c_str());
+                    fprintf_s(stderr, "%s option need argument.\n", in_argv_i.c_str());
                     return 0;
                 }
                 else
@@ -167,7 +188,7 @@ bool Command::__spaceArgSet(cmd_tuple &_Tuple,int argc,const char*argv[],int &i,
         { //出来ない場合
             if (std::get<DEF_VAL>(_Tuple).empty())
             { //デフォルト引数が設定されていない場合
-                fprintf_s(stderr, "%s option need argment.\n", in_argv_i.c_str());
+                fprintf_s(stderr, "%s option need argument.\n", in_argv_i.c_str());
                 return 0;
             }
             else
@@ -224,7 +245,7 @@ bool Command::__compound_short_name_parse(std::string &compound_short_name)
                 {
                     if (std::get<DEF_VAL>(f.second).empty())
                     {
-                        fprintf(stderr, "-%s option need argment.\n", target.c_str());
+                        fprintf(stderr, "%s option need argument.\n", target.c_str());
                         return 0;
                     }
                     else
@@ -241,90 +262,188 @@ bool Command::__compound_short_name_parse(std::string &compound_short_name)
 
 bool Command::_space_parse(int argc, const char *argv[])
 {
-
     for(int i = 1; i < argc; i++)
     {
         std::string in_argv_i(argv[i]);
         std::smatch sub_much;
-        if (isLongName(in_argv_i,sub_much))//long name
+        if (in_argv_i != "--")
         {
-            if (!__spaceIsMatch<LONG_NAME>(sub_much.str(1), argc, argv, i, in_argv_i))
+            if (isLongName(in_argv_i, sub_much)) //long name
             {
-                return 0;
+                if (!__spaceIsMatch<LONG_NAME>(sub_much.str(1), argc, argv, i, in_argv_i))
+                {
+                    return 0;
+                }
             }
-        }
-        else if(isShortName(in_argv_i,sub_much))//short name
-        {
-            if (!__spaceIsMatch<SHORT_NAME>(sub_much.str(1), argc, argv, i, in_argv_i))
+            else if (isShortName(in_argv_i, sub_much)) //short name
             {
-                return 0;
+                if (!__spaceIsMatch<SHORT_NAME>(sub_much.str(1), argc, argv, i, in_argv_i))
+                {
+                    return 0;
+                }
             }
-        }
-        else if(isCompoundShortName(in_argv_i,sub_much))//複合short name
-        {
-            if(!__compound_short_name_parse(sub_much.str(1)))
-            {
-                return 0;
-            }
-        }
-        else//simple string
-        {
-            if (!__simple_string_parse(in_argv_i))
-            {
-                return 0;
-            }
-        }
-    }//end for
-    return 1;
-}
-
-bool Command::_equal_parse(int argc, const char *argv[]){
-    
-    for(int i = 1; i < argc; i++)
-    {
-        std::string in_argv_i(argv[i]);
-        std::smatch sub_much;
-        if (isLongName(in_argv_i, sub_much)) //long
-        {
-            if (!__equalIsMatch<LONG_NAME>(sub_much.str(1), sub_much.str(3)))
-            {
-                return 0;
-            }
-        }
-        else if (isShortName(in_argv_i, sub_much)) //short
-        {
-            if (!__equalIsMatch<SHORT_NAME>(sub_much.str(1), sub_much.str(3)))
-            {
-                return 0;
-            }
-        }
-        else if (isCompoundShortName(in_argv_i, sub_much)) //compound short
-        {
-            if (sub_much.str(3).empty())
+            else if (isCompoundShortName(in_argv_i, sub_much)) //複合short name
             {
                 if (!__compound_short_name_parse(sub_much.str(1)))
                 {
                     return 0;
                 }
             }
-            else
+            else //simple string
             {
-                fprintf(stderr, "%s can not given argment.\n", in_argv_i.c_str());
-                return 0;
+                if (!__simple_string_parse(in_argv_i))
+                {
+                    return 0;
+                }
             }
         }
-        else// simple string
+        else if(accessable_next_argv(argc,i))
         {
+            in_argv_i = std::string(argv[++i]);
             if (!__simple_string_parse(in_argv_i))
             {
                 return 0;
             }
         }
+        else
+        {
+            fprintf_s(stderr,"Invalid '--' received.");
+            return 0;
+        }
+    }//end for
+    return 1;
+}
+
+bool Command::_equal_parse(int argc, const char *argv[])
+{
+    for(int i = 1; i < argc; i++)
+    {
+        std::string in_argv_i(argv[i]);
+        std::smatch sub_much;
+        if(in_argv_i != "--")
+        {
+            if (isLongName(in_argv_i, sub_much)) //long
+            {
+                if (!__equalIsMatch<LONG_NAME>(sub_much.str(1), sub_much.str(3)))
+                {
+                    return 0;
+                }
+            }
+            else if (isShortName(in_argv_i, sub_much)) //short
+            {
+                if (!__equalIsMatch<SHORT_NAME>(sub_much.str(1), sub_much.str(3)))
+                {
+                    return 0;
+                }
+            }
+            else if (isCompoundShortName(in_argv_i, sub_much)) //compound short
+            {
+                if (sub_much.str(3).empty())
+                {
+                    if (!__compound_short_name_parse(sub_much.str(1)))
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    fprintf(stderr, "%s can not given argument.\n", in_argv_i.c_str());
+                    return 0;
+                }
+            }
+            else // simple string
+            {
+                if (!__simple_string_parse(in_argv_i))
+                {
+                    return 0;
+                }
+            }
+        }
+        else if (accessable_next_argv(argc, i))
+        {
+            in_argv_i = std::string(argv[++i]);
+            if (!__simple_string_parse(in_argv_i))
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            fprintf_s(stderr, "Invalid '--' received.");
+            return 0;
+        }
+    }
+    return 1;
+}
+bool Command::_unix_parse(int argc, const char *argv[])
+{
+    for(int i = 1; i < argc; i++)
+    {
+        std::string in_argv_i(argv[i]);
+        std::smatch sub_much;
+        if (in_argv_i != "--")
+        {
+            if (isLongName(in_argv_i, sub_much)) //long
+            {
+                if (!__equalIsMatch<LONG_NAME>(sub_much.str(1), sub_much.str(3)))
+                {
+                    return 0;
+                }
+            }
+            else if (isShortName(in_argv_i, sub_much)) //short
+            {
+                if (!__spaceIsMatch<SHORT_NAME>(sub_much.str(1), argc, argv, i, in_argv_i))
+                {
+                    return 0;
+                }
+            }
+            else if (isCompoundShortName(in_argv_i, sub_much)) //compound short
+            {
+                if (sub_much.str(3).empty())
+                {
+                    if (!__compound_short_name_parse(sub_much.str(1)))
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    fprintf(stderr, "%s can not given argument.\n", in_argv_i.c_str());
+                    return 0;
+                }
+            }
+            else // simple string
+            {
+                if (!__simple_string_parse(in_argv_i))
+                {
+                    return 0;
+                }
+            }
+        }
+        else if (accessable_next_argv(argc, i))
+        {
+            in_argv_i = std::string(argv[++i]);
+            if (!__simple_string_parse(in_argv_i))
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            fprintf_s(stderr, "Invalid '--' received.");
+            return 0;
+        }
     }
     return 1;
 }
 
-Command &Command::flag(std::string tag_name, std::initializer_list<char> short_name, std::initializer_list<std::string> long_name, std::string description_message, bool need_arg, std::string default_val){
+Command &Command::argument(std::string tag_name, std::string description_message, std::string default_val)
+{
+    return flag(tag_name, {}, {},"",false,default_val);
+}
+
+Command &Command::flag(std::string tag_name, std::initializer_list<char> short_name, std::initializer_list<std::string> long_name, std::string description_message, bool need_arg, std::string default_val)
+{
     std::string str;
     if(short_name.size() != 0){
         str.push_back('(');
@@ -335,7 +454,7 @@ Command &Command::flag(std::string tag_name, std::initializer_list<char> short_n
         }
         str.pop_back();
         str.push_back(')');
-        fprintf_s(stderr,"%s\n",str.c_str());
+//        fprintf_s(stderr,"%s\n",str.c_str());
     }
     std::regex short_names(str.begin(), str.end());
     str.clear();
@@ -348,7 +467,7 @@ Command &Command::flag(std::string tag_name, std::initializer_list<char> short_n
         }
         str.pop_back();
         str.push_back(')');
-        fprintf_s(stderr, "%s\n", str.c_str());
+//        fprintf_s(stderr, "%s\n", str.c_str());
     }
     std::regex long_names(str.begin(), str.end());
     flag_items_m[tag_name] = std::make_tuple(short_names,long_names,description_message,need_arg,default_val,0,"");
@@ -362,9 +481,17 @@ template<class T>
 T Command::get(std::string tag_name){
     return T(std::get<ARG_VAL>(flag_items_m[tag_name]));
 }
+/*
+void Command::show_help(void)
+{
+    fprintf_s(stderr,"%s\n",get_help().c_str());
+}
 
-
-
+std::string Command::get_help(void)
+{
+    return app_name_m+arguments_text_m+options_text_m;
+}
+*/
 
 /* Templates Specialization */
 template<>
